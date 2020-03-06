1decc16ffae1b6d0dd2bfdc9385507faf35bfc50
Igor Tikhonin <ivtikhon@gmail.com>
2020-03-06 15:44:26 -0500

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# taskman
+# Taskman
+
+Taskman is a simple program to manage a list of tasks
+
+### Installation instructions
+
+
+git clone https://github.com/ivtikhon/taskman.git
+
+sudo apt-get -y install python3
+sudo apt-get -y install python3-pip
+sudo pip3 install flask
+sudo pip3 install flask-sqlalchemy
+sudo pip3 install tabulate
+
+### Usage
+
+cd taskman
+
+python startsever.py &
+
+#### List tasks
+
+python taskman.py list
+
+#### Add a task
+
+python taskman.py add "Develop Python program" "4/30/20"
+
+##### Delete task
+
+python taskman.py done 4
+
--- taskman.py
+++ taskman.py
@@ -3,28 +3,41 @@ import json
 from tabulate import tabulate
 import argparse
 import pprint
+from datetime import date, datetime
+
 
 API_ENDPOINT = 'http://127.0.0.1:5000/tasks/'   # Assign endpoint here
 
-def listTasks():
+def list_tasks(expring_today = False):
     res = requests.get(url = API_ENDPOINT)
     tasks = [['Id', 'Subject', 'Due date']]
+    today = datetime.now().date()
     if(res.status_code == 200):
         for task in res.json()['tasks']:
+            due_date = datetime.strptime(task['due_date'], "%m/%d/%Y").date()
+            if expring_today and due_date > today:
+                continue
             tasks.append([task['id'], task['subject'], task['due_date']])
-    print(tabulate(tasks, headers = 'firstrow'))
+    if len(tasks) > 1:
+        print(tabulate(tasks, headers = 'firstrow'))
 
-def addTask(subject, due_date = None):
+def add_task(subject, due_date = None):
     data = {'subject': subject}
     headers = {'content-type': 'application/json'}
     if due_date:
+        try:
+            # Check date format
+            date_time_obj = datetime.strptime(args.due_date, "%m/%d/%Y")
+        except:
+            print("Due date format is MM/DD/YYY")
+            exit(1)
         data['due_date'] = due_date
     res = requests.post(url = API_ENDPOINT, data = json.dumps(data), headers = headers)
     if res.status_code != 201:
         print('Failed to add task, see error log for details')
         exit(1)
 
-def delTask(id):
+def del_task(id):
     res = requests.delete(url = API_ENDPOINT + '/' + str(id))
     if res.status_code != 200:
         print('Failed to delete task, see error log for details')
@@ -33,21 +46,24 @@ def delTask(id):
 if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser(prog='taskman.py')
-    subparsers = parser.add_subparsers(dest='command', help='commands')
-    list_parser = subparsers.add_parser('list', help='List tasks')
+    subparsers = parser.add_subparsers(dest='command', help ='commands')
+    list_parser = subparsers.add_parser('list', help = 'list tasks')
+    list_parser.add_argument('--expiring-today', help = 'tasks that are due today', action='store_true')
 
-    create_parser = subparsers.add_parser('add', help='Add a task')
-    create_parser.add_argument('subject', action='store', help='Task subject')
-    create_parser.add_argument('due_date', action='store', help='Due date')
+    create_parser = subparsers.add_parser('add', help='add a task')
+    create_parser.add_argument('subject', action='store', help='task subject')
+    create_parser.add_argument('due_date', action='store', help='due date')
 
-    delete_parser = subparsers.add_parser('done', help='Close a task')
-    delete_parser.add_argument('id', action='store', help='Id of the task to close')
+    delete_parser = subparsers.add_parser('done', help='close a task')
+    delete_parser.add_argument('id', action='store', help='id of the task to close')
 
     args = parser.parse_args()
 
+    # Process commands
     if args.command == 'add':
-        addTask(subject = args.subject, due_date = args.due_date)
+        date_time_obj = None
+        add_task(subject = args.subject, due_date = args.due_date)
     elif args.command == 'list':
-        listTasks()
+        list_tasks(args.expiring_today)
     elif args.command == 'done':
-        delTask(args.id)
+        del_task(args.id)
