c96ee17a6e24d54a04c0b6c906ad83240f69a1c0
Igor Tikhonin <ivtikhon@gmail.com>
2020-03-06 13:47:20 -0500

    Added argument parsing

--- taskman.py
+++ taskman.py
@@ -1,11 +1,10 @@
 import requests
 import json
 from tabulate import tabulate
+import argparse
+import pprint
 
-API_ENDPOINT = 'http://127.0.0.1:5000/tasks/'
-
-def printTask(task):
-    print('id: ' + str(task['id']) + ' subject: ' + task['subject'] + ' due date: ' + task['due_date'])
+API_ENDPOINT = 'http://127.0.0.1:5000/tasks/'   # Assign endpoint here
 
 def listTasks():
     res = requests.get(url = API_ENDPOINT)
@@ -15,17 +14,40 @@ def listTasks():
             tasks.append([task['id'], task['subject'], task['due_date']])
     print(tabulate(tasks, headers = 'firstrow'))
 
-def addTask(subject, due = None):
+def addTask(subject, due_date = None):
     data = {'subject': subject}
     headers = {'content-type': 'application/json'}
-    if due:
-        data['due_date'] = due
+    if due_date:
+        data['due_date'] = due_date
     res = requests.post(url = API_ENDPOINT, data = json.dumps(data), headers = headers)
     if res.status_code != 201:
         print('Failed to add task, see error log for details')
         exit(1)
 
+def delTask(id):
+    res = requests.delete(url = API_ENDPOINT + '/' + str(id))
+    if res.status_code != 200:
+        print('Failed to delete task, see error log for details')
+        exit(1)
 
 if __name__ == '__main__':
-    # addTask("Collect input from stakeholders for weekly status meeting", "4/8/2020")
-    listTasks()
+    # Parse arguments
+    parser = argparse.ArgumentParser(prog='taskman.py')
+    subparsers = parser.add_subparsers(dest='command', help='commands')
+    list_parser = subparsers.add_parser('list', help='List tasks')
+
+    create_parser = subparsers.add_parser('add', help='Add a task')
+    create_parser.add_argument('subject', action='store', help='Task subject')
+    create_parser.add_argument('due_date', action='store', help='Due date')
+
+    delete_parser = subparsers.add_parser('done', help='Close a task')
+    delete_parser.add_argument('id', action='store', help='Id of the task to close')
+
+    args = parser.parse_args()
+
+    if args.command == 'add':
+        addTask(subject = args.subject, due_date = args.due_date)
+    elif args.command == 'list':
+        listTasks()
+    elif args.command == 'done':
+        delTask(args.id)
