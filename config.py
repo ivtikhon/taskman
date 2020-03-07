820e64434da9ffc15c6da9786ef83e904bd5f4db
Igor Tikhonin <ivtikhon@gmail.com>
2020-03-07 15:12:21 -0500

    Configured server name

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.getcwd(), 'taskman.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SERVER_NAME = '127.0.0.1:5000'
 
 
 class TestingConfig:
